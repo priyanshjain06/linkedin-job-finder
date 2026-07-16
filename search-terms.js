8b0af18cf7a082b759c94e879698ffb2704d7d93
Priyansh Jain <priyansh01jain@gmail.com>
2026-07-17 00:23:02 +0530

    added comments !!

--- search-terms.js
+++ search-terms.js
@@ -7,23 +7,31 @@ export const generateSearchTerms = () => {
     'Gurgaon'
   ]
 
-  //REVIEW Location 
   // 'Bangalore',
   //   'Pune',
   //   'Nodia',  
   //   'Delhi NCR',
 
+  
   const titles = ['0 years of experience full stack engineer', '0 years of experience front end engineer', '0 years of experience back end engineer']
 
-  //REVIEW -  Titles 
-  // const titles = ['SDE Intern',"Software Intern","Software Engineer Intern","Software Developer Intern","Software Development Engineer Intern","Software Development Intern",'Software Engineer Trainee',"Software Developer Trainee","Software Development Engineer Trainee","Software Development Trainee"]
-  
 
-  //REVIEW time 
-  //const time = '12 hours'
-  //const time = '2 hours'
-  //const time = '1 week'
+  //   const titles = [
+  //   "Software Engineer Intern",
+  //   "SDE Intern",
+  //   "Software Developer Intern",
+  //   "Front End Intern",
+  //   "Back End Intern"
+  // ]
+
+
+  // const titles = ['SDE Intern',"Software Intern","Software Engineer Intern","Software Developer Intern","Software Development Engineer Intern","Software Development Intern",'Software Engineer Trainee',"Software Developer Trainee","Software Development Engineer Trainee","Software Development Trainee"]
+
+
+  // const titles = ["Front End Intern","Front End Software Engineer Intern","Front End Software Developer Intern","Front End Software Development Intern","Back End Intern","Back End Software Engineer Intern","Back End Software Developer Intern","Back End Software Development Intern","Full Stack Intern","Full Stack Software Engineer Intern","Full Stack Software Developer Intern","Full Stack Software Development Intern"]
+
   
+  //const time = '2 hours'
   const time = '24 hours'
 
   return cities.map(city => ({
