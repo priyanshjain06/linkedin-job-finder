a4d41cf309276082c0763c7eeab28e3daadb113d
Priyansh Jain <priyansh01jain@gmail.com>
2026-07-17 00:26:03 +0530

    correcte the  file structure !!

---
+++ extra-titles.js
@@ -0,0 +1,4 @@
+  // const titles = ['SDE Intern',"Software Intern","Software Engineer Intern","Software Developer Intern","Software Development Engineer Intern","Software Development Intern",'Software Engineer Trainee',"Software Developer Trainee","Software Development Engineer Trainee","Software Development Trainee"]
+
+
+  // const titles = ["Front End Intern","Front End Software Engineer Intern","Front End Software Developer Intern","Front End Software Development Intern","Back End Intern","Back End Software Engineer Intern","Back End Software Developer Intern","Back End Software Development Intern","Full Stack Intern","Full Stack Software Engineer Intern","Full Stack Software Developer Intern","Full Stack Software Development Intern"]
